adad973e8f3f03956048402b3cada6139bc976cd
davjgardner <davjgardner@gmail.com>
2017-10-22 11:03:46 -0400

    Create Plane.java

---
+++ src/geom/Plane.java
@@ -0,0 +1,31 @@
+package geom;
+
+import org.joml.Vector3f;
+
+/**
+ * Created by david on 10/22/2017.
+ */
+public class Plane extends Shape {
+	public static final float EPSILON = 0.00001f;
+	
+	public Vector3f normal, pos;
+	
+	public Plane(Vector3f normal, Vector3f pos, Material m) {
+		this.normal = normal;
+		this.pos = pos;
+		this.m = m;
+	}
+	
+	@Override
+	public float collides(Ray ray) {
+		// For now, assume any parallel rays do not intersect
+		float d = ray.direction.dot(normal);
+		if (Math.abs(d) < EPSILON) return -1;
+		return normal.dot(new Vector3f(ray.origin).sub(pos)) / normal.dot(ray.direction);
+	}
+	
+	@Override
+	public Vector3f normalAt(Vector3f pos) {
+		return normal;
+	}
+}
